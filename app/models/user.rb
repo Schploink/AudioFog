fe348d72735b083dd171a3a43907d1f48b1ff7ca
Schploink <thekpoc@gmail.com>
2021-06-01 16:38:50 -0700

    User model auth functionality

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,39 @@
 class User < ApplicationRecord
+  vaildates :password_digest, presence: true
+  validates :username, :email, :session_token, presence: true, uniqueness: true
+  validates :password, length: {minimum: 6}, allow_nil: true
+  before_validation :ensure_session_token
 
-  
+  def generate_session_token
+    SecureRandom::base64
+  end
+
+  def password=(password)
+    self.password_digest = BCrypt::Password.create(password)
+    @password = password
+  end
+
+  def ensure_session_token 
+    self.session_token ||= generate_session_token 
+  end
+
+  def reset_session_token!
+    self.session_token = generate_session_token
+    self.save!
+    self.session_token
+  end
+
+  def correct_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?()
+  end
+
+  def self.find_by_credentials(username, password)
+    user = User.find_by(username: username)
+
+    if user && user.correct_password?(password)
+      return user
+    else
+      return nil
+    end
+  end
 end
