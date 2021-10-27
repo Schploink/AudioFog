25d343ccb62fbf6210792395c14360b6309bcef8
Schploink <THEKPOC@Gmail.com>
2021-10-27 11:07:26 -0700

    add icons

--- frontend/components/player/player.jsx
+++ frontend/components/player/player.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import {IoPlaySkipBack} from "react-icons/io5"
+import {IoPlaySkipForward} from "react-icons/io5"
+import {IoPlay} from "react-icons/io5"
+import {IoPause} from "react-icons/io5"
 
 
 class Player extends React.Component {
 	constructor(props) {
 		super(props);
+
+    this.state = {
+      isPlaying : false
+    }
+
 	}
 
 
@@ -11,31 +20,40 @@ class Player extends React.Component {
 	render() {
 		return (
 				<div className="player-container">
-          Thisistheplayer
-        <audio src="https://active-storage-audiofog-dev.s3.us-west-1.amazonaws.com/01+Body+Electric.mp3" preload="metadata">
+          <div>
+          
+            <audio src="https://active-storage-audiofog-dev.s3.us-west-1.amazonaws.com/01+Body+Electric.mp3" preload="metadata">
 
-        </audio>
-        <button>
-          play/pause
-        </button>
-        {/* current time */}
-        <div>
-          0:00
-        </div>
+            </audio>
 
-        {/* progress bar */}
-        <div>
-          <input type="range" />
-        </div>
+            <button className="previous-track">
+              <IoPlaySkipBack />
+            </button>
 
-        {/* duration */}
-        <div>
-          5:40
-        </div>
-        
-          {/* <audio controls>
-            <source src="https://active-storage-audiofog-dev.s3.us-west-1.amazonaws.com/01+Body+Electric.mp3" type="audio/mpeg" />
-          </audio> */}
+            <button className="play/pause">
+              { isPlaying ? <IoPause /> : <IoPlay />}
+            </button>
+
+            <button className="next-track">
+              <IoPlaySkipForward />
+            </button>
+            
+            <div className="current-time">
+              0:00
+            </div>
+
+            <div className="progress-bar">
+              <input type="range" />
+            </div>
+
+            <div className="time-remaining">
+              5:40
+            </div>
+          
+          </div>
+            {/* <audio controls>
+              <source src="https://active-storage-audiofog-dev.s3.us-west-1.amazonaws.com/01+Body+Electric.mp3" type="audio/mpeg" />
+            </audio> */}
 				</div>
 		)}
 }
