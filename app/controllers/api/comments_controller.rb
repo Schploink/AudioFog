a66f0c9c4f61e1b03d8b0127172bb30f1cdd1ab7
Schploink <THEKPOC@Gmail.com>
2021-11-19 13:42:07 -0800

    create comment controller

---
+++ app/controllers/api/comments_controller.rb
@@ -0,0 +1,27 @@
+class Api::CommentsController < ApplicationController
+
+    def create
+        @comment = Comment.new(comment_params)
+        @comment.author_id = current_user.id
+
+        if @comment.save
+            render :show
+        else
+            render json: @comment.errors.full_messages, status: 422
+        end
+    end
+
+    def destroy
+        @comment = Comment.find(params[:id])
+        @comment.destroy 
+        render json: {}
+    end
+
+
+    private
+
+    def comment_params
+        params.require(:comment).permit(:body, :sound_id)
+    end
+
+end
