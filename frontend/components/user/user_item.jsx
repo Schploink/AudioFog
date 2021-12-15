2cdf1a98256f3e900dd4d9cb42a7f9d74bc66eff
Schploink <THEKPOC@Gmail.com>
2021-12-14 17:13:06 -0800

    sound item presentable and functional

--- frontend/components/user/user_item.jsx
+++ frontend/components/user/user_item.jsx
@@ -2,6 +2,7 @@ import { Link } from 'react-router-dom'
 import React from 'react';
 import {IoPlay} from "react-icons/io5"
 import {IoPause} from "react-icons/io5"
+import {RiDeleteBin7Line} from "react-icons/ri"
 
 class UserSongItem extends React.Component {
 
@@ -33,13 +34,24 @@ class UserSongItem extends React.Component {
     render() {
         
         let sound = this.props.sound
+        const deleteButton = sound.uploader_id === this.props.currentUser
+            ? <div 
+                className="comment-delete-button"
+                // Sound delete disabled in production to maintain site integrity
+                // onClick={() => this.props.deleteSound(sound.id)}
+                >
+                    <RiDeleteBin7Line />
+                </div>
+            : <div></div>
         
         return (
             <div className="user-song-item">
-                <img 
-                className="user-song-art"
-                src={sound.photoUrl} 
-                />
+                <Link to={`/sounds/${sound.id}`}>
+                    <img 
+                    className="user-song-art"
+                    src={sound.photoUrl} 
+                    />
+                </Link>
                 <div className="info-waveform">
                     <div className="play-button-title">
                         <div className="play-button-background"
@@ -52,18 +64,18 @@ class UserSongItem extends React.Component {
                             <div className="user-item-artist-name">
                                 {sound.artist}
                             </div>
-                            <div className="user-item-sound-title">
-                                {sound.description}
-                            </div>
+                            <Link to={`/sounds/${sound.id}`}>
+                                <div className="user-item-sound-title">
+                                    {sound.description}
+                                </div>
+                            </Link>
                         </div>
+                        {deleteButton}
                     </div>
                     <img 
                     className="user-waveform" 
                     src={sound.waveUrl}
                     />
-                </div>
-                <div>
-                    Delete Button
                 </div>
             </div>
         )
--- frontend/components/user/user_item_container.jsx
+++ frontend/components/user/user_item_container.jsx
@@ -1,18 +1,19 @@
 import { connect } from 'react-redux';
 import UserItem from './user_item'
-import { fetchAllSounds } from '../../actions/sound_actions';
+import { fetchAllSounds, destroySound } from '../../actions/sound_actions';
 import {receiveCurrentSound, playSound, pauseSound} from "../../actions/playstate_actions"
 
 const mapStateToProps = (state) => ({
   sounds: Object.values(state.entities.sounds),
   currentSound: state.entities.sounds[state.ui.currentSound.id],
-  currentUser: state.entities.users[state.session.id],
+  currentUser: state.session.id,
   users: state.entities.users,
   isPlaying: state.ui.isPlaying
 });
 
 const mapDispatchToProps = (dispatch) => ({
   fetchAllSounds: () => dispatch(fetchAllSounds()),
+  deleteSound: (soundID) => dispatch(destroySound(soundID)),
   playSound: () => dispatch(playSound()),
   pauseSound: () => dispatch(pauseSound()),
   receiveCurrentSound: (soundID) => dispatch(receiveCurrentSound(soundID))
